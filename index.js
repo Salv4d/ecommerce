2c0b91e9da992eb207fbaaa658f79d9c9ab01267
Salvador Oliveira <slsalvadorlucas@gmail.com>
2021-11-16 22:42:04 -0400

    Add sign up form

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('How are you doing today?')
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="repeat password" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post("/", (req, res) => {
+  res.send("Account created!");
+  console.log(req.body);
+});
 
 app.listen(3000, () => {
-  console.log('Listening');
+  console.log("Listening");
 });
