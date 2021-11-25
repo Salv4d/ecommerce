2d1203e095b3c01fcba7394a7e51adbc8ef41c1a
Salvador Oliveira <slsalvadorlucas@gmail.com>
2021-11-24 20:52:53 -0400

    Add layout to admin panel views

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require("../layout");
+
 module.exports = () => {
-  return `
-  <div>
-      <form method="POST">
-        <input name="name" placeholder="name" />
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="repeat password" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `;
+  return layout({
+    content: `
+      <div>
+        <form method="POST">
+          <input name="name" placeholder="name" />
+          <input name="email" placeholder="email" />
+          <input name="password" placeholder="password" />
+          <input name="passwordConfirmation" placeholder="repeat password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+  `,
+  });
 };
